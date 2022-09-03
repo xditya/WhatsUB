8065edf0c55d6bd01f9a69b3089fab611a2708b3
Aditya <xditya@ultroid.tech>
2022-09-03 14:19:12 +0530

    more stats

--- modules/stats.js
+++ modules/stats.js
@@ -1,4 +1,4 @@
-const { PrivateChat, GroupChat } = require("whatsapp-web.js");
+const { PrivateChat, GroupChat, ClientInfo } = require("whatsapp-web.js");
 
 const execute = async (client, msg, args) => {
   await msg.delete(true);
@@ -6,6 +6,7 @@ const execute = async (client, msg, args) => {
   let pms = 0,
     groups = 0,
     unlisted = 0;
+  const me = await client.info;
   for (let i = 0; i < chats.length; i++)
     if (chats[i] instanceof PrivateChat) pms++;
     else if (chats[i] instanceof GroupChat) groups++;
@@ -13,11 +14,26 @@ const execute = async (client, msg, args) => {
       console.log(chats[i]);
       unlisted++;
     }
-  var txt = `*Total Chats*: ${chats.length}
-
-• *PMs*: ${pms}
-• *Groups*: ${groups}`;
-  if (unlisted != 0) txt += `\n• *UnListed*: ${unlisted}`;
+  var txt =
+    `*Name*: ` +
+    "```" +
+    `${me.pushname}` +
+    "```" +
+    `
+*Server*: ` +
+    "```" +
+    `${me.me.server}` +
+    "```" +
+    `
+*Platform*: ` +
+    "```" +
+    `${me.platform}` +
+    "```" +
+    `
+*Total Chats*: ${chats.length}
+    • *PMs*: ${pms}
+    • *Groups*: ${groups}`;
+  if (unlisted != 0) txt += `\n    • *UnListed*: ${unlisted}`;
   await msg.reply(txt);
 };
 
